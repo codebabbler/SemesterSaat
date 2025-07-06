d3f7784a65d3a73660e5604c274f19664d946e63
blackkspydo <blackkspydo@gmail.com>
2025-07-06 23:51:08 +0545

    Refactor Modal component styles and improve global CSS variables for better consistency and maintainability.

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -13,24 +13,22 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, title }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50">
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
+    <div className="bg-opacity-50 fixed top-0 right-0 left-0 z-50 flex h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-x-hidden overflow-y-auto bg-black/20">
+      <div className="relative max-h-full w-full max-w-2xl p-4">
         {/* Modal content */}
-        <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+        <div className="relative rounded-lg bg-white shadow-sm">
           {/* Modal header */}
 
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-            <h3 className="text-lg font-medium text-gray-900 dark:text-white">
-              {title}
-            </h3>
+          <div className="flex items-center justify-between rounded-t border-b border-gray-200 p-4 md:p-5">
+            <h3 className="text-lg font-medium text-gray-900">{title}</h3>
 
             <button
               type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+              className="inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
               onClick={onClose}
             >
               <svg
-                className="w-3 h-3"
+                className="h-3 w-3"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -48,9 +46,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, title }) => {
           </div>
 
           {/* Modal body */}
-          <div className="p-4 md:p-5 space-y-4">
-           {children}
-          </div>
+          <div className="space-y-4 p-4 md:p-5">{children}</div>
         </div>
       </div>
     </div>
