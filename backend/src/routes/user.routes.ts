78ebde560be1d66e7efd6a5e419874662cce5e0e
blackkspydo <blackkspydo@gmail.com>
2025-07-10 23:33:11 +0545

    feat: Add user profile and password management routes
    - Implemented routes for changing password, retrieving user profile, and updating user profile.
    - Secured new routes with JWT verification for enhanced user authentication.

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,6 +4,9 @@ import {
   registerUser,
   logoutUser,
   refreshAccessToken,
+  changePassword,
+  getUserProfile,
+  updateUserProfile,
 } from "../controllers/user.controller";
 import { verifyJWT } from "../middleware/auth.middleware";
 const router: Router = Router();
@@ -15,5 +18,10 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").get(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changePassword);
+router
+  .route("/profile")
+  .get(verifyJWT, getUserProfile)
+  .put(verifyJWT, updateUserProfile);
 
 export default router;
