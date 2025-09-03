84fb50b1cda94292f0c6bae107221b44de5609e3
blackkspydo <blackkspydo@gmail.com>
2025-09-03 21:03:59 +0545

    refactor: Simplify AddIncomeForm by removing prediction logic and enhancing input handling
    - Removed prediction and feedback features from the AddIncomeForm component to streamline the user experience.
    - Updated input handling for income source and description fields for better clarity and usability.
    - Adjusted the layout of the form to improve overall structure and maintainability.

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -105,7 +105,7 @@ const SignUpForm = () => {
         </p>
 
         <form onSubmit={handleSignUp}>
-          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+          
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <Input
